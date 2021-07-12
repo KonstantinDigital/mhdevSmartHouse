32127f7ac803e2a24a888f99be014e504d3525f6
KonstantinDigital <martyrhartman@yandex.ru>
2021-07-12 14:58:01 +0300

    12.07.2021-15:00

--- smartHouseApp/urls.py
+++ smartHouseApp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    url('favicon.ico', RedirectView.as_view(url='/static/images/favicon.ico')),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('login/', LoginView.as_view(), name='login'),
     path('', views.index, name='index'),
