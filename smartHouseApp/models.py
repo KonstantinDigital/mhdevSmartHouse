5b11349f73f55398372d6c005cda4151866d1087
KonstantinDigital <martyrhartman@yandex.ru>
2021-07-14 11:26:07 +0300

    14.07.2021-11:30-onWork

--- smartHouseApp/models.py
+++ smartHouseApp/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+class SmartHouseUser(User):
+    class Meta:
+        permissions = [("can_set_temperature", "Can set temperature"), ("can_set_light", "Can set light")]
